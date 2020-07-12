b71029695c61b81917040d5f6ec51937dce7b70c
Zkoiran <122123yc>
2020-07-12 17:43:21 +0700

    Complitely done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean1 = (Cat) applicationContext.getBean("cat");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean.equals(bean2));
+        System.out.println(bean1.equals(bean4));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String setMessage(String message) {
+        return this.message = message;
+    }
+}
